fbaf81ad3ba203b4c0599d3346ebd03571198950
Kevin Hooke <kevin.hooke@gmail.com>
2016-01-31 13:47:34 -0800

    initial

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# SimpleAngularJSExample-ngRoute
-Simplest case example of an AngularJS app using ngRoute to demonstrate
-loading of page templates and controllers based on URL hash fragments.
+# SimpleAngularJSExample-uirouter
+Simplest case example of an AngularJS app using AngularUI ui-router to demonstrate
+view templates with submenus and nested views.
+
+For comparison, also see my example using AngularJS ngRoute.
 
 Created while learning AngularJS, so not intended to be an example of best practices, just a simple example for future reference.
--- js/simpleapp.js
+++ js/simpleapp.js
@@ -1,26 +1,38 @@
-var simpleApp = angular.module('SimpleApp',
-	[
-	 	"ngRoute",
-		"SimpleControllers"
-	]);
+var simpleApp = angular.module('SimpleUIRouterApp', 
+    [ "ui.router", "ngAnimate", "SimpleControllers" ]);
 
 
-simpleApp.config(['$routeProvider',
-    function($routeProvider) {
-      $routeProvider.
-      when('/', {
-          templateUrl: 'page1.html',
-          controller: 'Page1Controller'
-        }).
-        when('/page2', {
-          templateUrl: 'page2.html',
-          controller: 'Page2Controller'
-        }).
-        when('/page3', {
-          templateUrl: 'page3.html',
-          controller: 'Page3Controller'
-        }).
-        otherwise({
-          redirectTo: '/'
-        });
-    }]);
+simpleApp.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    // show page 1
+    .state('page1', {
+      url: '/page1',
+      templateUrl: 'page1.html'
+    })
+    
+    .state('page2', {
+      url: '/page2',
+      templateUrl: 'page2/page2.html'
+    })
+
+    .state('page2.nested1', {
+      url: '/page2nested1',
+      templateUrl: 'page2/page2_subsection1.html'
+    })
+
+    .state('page2.nested2', {
+      url: '/page2nested2',
+      templateUrl: 'page2/page2_subsection2.html'
+    })
+
+    .state('page2.nested3', {
+      url: '/page2nested3',
+      templateUrl: 'page2/page2_subsection3.html'
+    })
+
+    .state('page3', {
+      url: '/page3',
+      templateUrl: 'page3/page3.html'
+    });
+});
